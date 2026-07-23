2fdf724eee47f4ec9d18110ba9ef179d6b792a1f
soham <hsohamsanat@gmail.com>
2026-07-23 15:24:23 +0530

    docs: add modern README, fix vintage pack drop rates, and clean repo junk

--- FrontEnd/PackOpeningUI/test_fetch.mjs
+++
@@ -1,9 +0,0 @@
-import { fetchJapaneseSeriesDetails } from './src/app/services/scrydex.js';
-
-fetchJapaneseSeriesDetails('sv_ja')
-  .then(res => console.log('sv_ja', res.sets.length))
-  .catch(console.error);
-
-fetchJapaneseSeriesDetails('swsh_ja')
-  .then(res => console.log('swsh_ja', res.sets.length))
-  .catch(console.error);
